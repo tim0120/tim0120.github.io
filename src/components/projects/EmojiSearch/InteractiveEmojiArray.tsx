671145c0def3c1cf59c36d77113ca3811d8cd7af
Timothy Kostolansky <39891386+tim0120@users.noreply.github.com>
2024-11-23 00:06:11 -0800

    width-based grid shapes, larger emoji sizing

--- src/components/projects/EmojiSearch/InteractiveEmojiArray.tsx
+++ src/components/projects/EmojiSearch/InteractiveEmojiArray.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+import clsx from 'clsx';
 import InteractiveEmoji from "./InteractiveEmoji";
 
 interface Props {
@@ -9,9 +11,27 @@ export default function InteractiveEmojiArray({
   emojiList,
   onEmojiClick
 }: Props) {
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 640);
+
+  useEffect(() => {
+    const updateGrid = () => {
+      setIsSmallScreen(window.innerWidth < 640);
+    };
+
+    updateGrid();
+    window.addEventListener('resize', updateGrid);
+    return () => window.removeEventListener('resize', updateGrid);
+  }, []);
+
   return (
     <>
-      <div className="text-3xl sm:text-4xl grid grid-cols-10 grid-rows-3 gap-2">
+      <div className={clsx(
+        "text-5xl sm:text-5xl grid gap-4 sm:gap-4",
+        {
+          'grid-cols-6 grid-rows-5': isSmallScreen,
+          'grid-cols-10 grid-rows-3': !isSmallScreen
+        }
+      )}>
         {emojiList.map((emoji, i) => (
           <div key={i} className="flex justify-center">
             <InteractiveEmoji emojiChar={emoji} onEmojiClick={onEmojiClick} />
