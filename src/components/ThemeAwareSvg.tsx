b7208fcd20bfe81064b0d3de40a69be81f6347a4
Timothy Kostolansky <39891386+tim0120@users.noreply.github.com>
2025-09-19 17:45:43 -0700

    better svg loading (hardcoding lol)

--- src/components/ThemeAwareSvg.tsx
+++ src/components/ThemeAwareSvg.tsx
@@ -1,16 +1,37 @@
 "use client"
 
+import React from "react";
 import Image from "next/image";
 
+const INLINE_ICONS: Record<string, string> = {
+  "/images/file-text.svg": `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>`,
+  "/images/github.svg": `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>`,
+  "/images/twitter.svg": `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>`,
+  "/images/linkedin.svg": `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>`,
+  "/images/googlescholar.svg": `<svg role="img" viewBox="-1 -1 26 26" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><title>Google Scholar</title><path d="M5.242 13.769L0 9.5 12 0l12 9.5-5.242 4.269C17.548 11.249 14.978 9.5 12 9.5c-2.977 0-5.548 1.748-6.758 4.269zM12 10a7 7 0 1 0 0 14 7 7 0 0 0 0-14z"/></svg>`
+};
+
 const ThemeAwareSvg = ({ src, className = '' }: { src: string; className?: string }) => {
+  const rawSvg = INLINE_ICONS[src];
+
+  if (!rawSvg) {
+    return (
+      <Image
+        src={src}
+        alt=""
+        width={24}
+        height={24}
+        className={`inline-block w-6 h-6 ${className}`}
+        aria-hidden="true"
+      />
+    );
+  }
+
   return (
-    <Image
-      src={src}
-      alt=""
-      width={24}
-      height={24}
-      className={`inline-block w-6 h-6 ${className}`}
+    <span
+      className={`inline-block align-middle w-6 h-6 ${className}`}
       aria-hidden="true"
+      dangerouslySetInnerHTML={{ __html: rawSvg }}
     />
   );
 };
