3a67adb1bbd3636c3d105777a27de3b94bf5a1ea
Timothy Kostolansky <39891386+tim0120@users.noreply.github.com>
2025-09-26 20:44:02 -0700

    project rect nicer

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,12 +8,10 @@ export default function ProjectsPage() {
 	  <div className="flex flex-col items-start">
 			<h1 className="text-3xl font-bold mb-4">Projects</h1>
 			{projects.map((project) => (
-				<div key={project.slug} className="mb-4 mt-4 border border-transparent rounded-lg hover:border-gray-300 p-4">
-					<Link href={`/projects/${project.slug}`}>
-						<h2 className="text-xl font-bold">{project.title}</h2>
-						<p>{project.description}</p>
-					</Link>
-				</div>
+				<Link key={project.slug} href={`/projects/${project.slug}`} className="block mb-4 mt-4 border border-gray-300 rounded-lg p-4 hover:border-gray-400 hover:shadow-[0_0_15px_rgba(0,0,0,0.1)] hover:dark:shadow-[0_0_15px_rgba(255,255,255,0.3)] transition-shadow duration-100">
+					<h2 className="text-xl font-bold">{project.title}</h2>
+					<p>{project.description}</p>
+				</Link>
 			))}
 	  </div>
 	</div>
