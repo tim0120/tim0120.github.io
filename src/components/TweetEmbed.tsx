4e03fcfcf091bc83e58d1e502fd63087fe63455a
Timothy Kostolansky <39891386+tim0120@users.noreply.github.com>
2025-10-21 17:05:45 -0400

    Replace Twitter widget with react-tweet

--- src/components/TweetEmbed.tsx
+++ src/components/TweetEmbed.tsx
@@ -1,30 +1,13 @@
-'use client';
-
-import { useEffect } from 'react';
+import { Tweet } from 'react-tweet';
 
 interface TweetEmbedProps {
   tweetId: string;
 }
 
 export default function TweetEmbed({ tweetId }: TweetEmbedProps) {
-  useEffect(() => {
-    // Load Twitter widget script
-    const script = document.createElement('script');
-    script.src = 'https://platform.twitter.com/widgets.js';
-    script.async = true;
-    document.body.appendChild(script);
-
-    return () => {
-      // Cleanup
-      document.body.removeChild(script);
-    };
-  }, []);
-
   return (
-    <div className="flex justify-center max-w-md mx-auto">
-      <blockquote className="twitter-tweet" data-theme="dark">
-        <a href={`https://twitter.com/x/status/${tweetId}`}></a>
-      </blockquote>
+    <div className="flex justify-center">
+      <Tweet id={tweetId} />
     </div>
   );
 }
